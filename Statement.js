ec2701a80e2330ce8010736de89cacfbef306190
fridayshoes <one_amp@hotmail.com>
2022-07-28 09:06:03 +0100

    Removed console.logs and redundant code

--- Statement.js
+++ Statement.js
@@ -19,13 +19,11 @@ class Statement {
   }
   
   getBalance() {
-    // let balance = 0;
-    // console.log(4)
     this.transactionList.forEach ((transaction) => {
       if(transaction.transactionType === "deposit")
         this.balance += transaction.amount; // adds to balance if transactionType is deposit
       else
-        this.balance -= transaction.amount; // subtracts from balance if transactionType is withdrawal
+        this.balance -= transaction.amount; // subtracts from balance if transactionType is withdraw
         Object.assign(transaction, { balance: this.balance }) // assigns a k/v to the object with the value of the current balance
     })
   }
@@ -48,30 +46,7 @@ class Statement {
 module.exports = Statement;
 
 
-
-// Tests date is formatted correctly
-// transactionListBefore = new TransactionDetails("10-01-2023", "deposit", 1000.00);
-// console.log(transactionListBefore.getDate());
-// console.log(transactionListBefore);
-
-// Tests transactions are saved in a list
-// console.log(1)
-// allTransactions = new Statement();
-// allTransactions.addTransaction(new TransactionDetails("10-01-2023", "deposit", 1000.00));
-// allTransactions.addTransaction(new TransactionDetails("13-01-2023", "deposit", 2000.00));
-// allTransactions.addTransaction(new TransactionDetails("14-01-2023", "withdraw", 500.00));
-// console.log(allTransactions);
-// // console.log(allTransactions.getBalance());
-// console.log(2)
-// allTransactions.deposit(500, "27-07-2022");
-
-// console.log(allTransactions.createStatement());
-
-
-// console.log("Print Statement");
-// console.log(printStatement(transactions));
-// returns
-//
+// Example result
 // date       || credit  || debit  || balance
 // 14/01/2023 ||         || 500.00 || 2500.00
 // 13/01/2023 || 2000.00 ||        || 3000.00
